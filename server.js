153df6d6e9d1e2506575b8b17b8213157e6b2ebc
刘浩 <liuhao@icourt.cc>
2018-09-05 21:08:40 +0800

    :sparkles:重新规划了代码结构，完善了原生Ajax上传逻辑，可以预览上传图片，但是还存在服务端多文件上传bug待修复

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ http.createServer((req, res) => {
     if (req.url.indexOf('/download') > -1) {
         res.setHeader('Access-Control-Allow-Origin', '*');
         let filename = req.url.substring(req.url.lastIndexOf('/') + 1);
-        fs.readFile(`C:\\Users\\patrick.liu\\Desktop\\ajax\\${filename}`, 'utf-8', (err, data) => {
+        fs.readFile(`.\\${filename}`, 'utf-8', (err, data) => {
             if (err) {
                 res.end(err.message);
             } else {
@@ -41,7 +41,8 @@ http.createServer((req, res) => {
                 buf = Buffer.from(chunks[0]);
             }
             let rems = [];
-            // 根据/r/n来拆分数据和报头，其中/r的ASCII为13，/n的ASCII为10
+            // 根据/r/n来拆分数据和报头，其中/r的ASCII为13，/n的ASCII为10，由于文件首位照应，所以需要那前后一致对内容来做匹配，其他内容作为buffer写出
+            // 以下添加序列字符+2都是为了去掉/r/n的分割
             // multipart/form-data上传的数据格式
             // ------WebKitFormBoundarytBtkoYcpqn4hvqSC
             // Content-Disposition: form-data; name="file"; filename="log.txt"
@@ -55,19 +56,41 @@ http.createServer((req, res) => {
                     rems.push(i);
                 }
             }
+            // 按照每6个进行解析
+            console.log('rems', rems);
+            for (let i = 0; i < rems.length; i = i + 5) {
+                // 获取Content-Disposition行
+                let contentDisposition =  buf.slice(rems[i] + 2, rems[i+1]).toString();
+                console.log('contentDisposition', contentDisposition);
+                let filename = contentDisposition.substring(contentDisposition.indexOf('filename=') + 10 , contentDisposition.length - 1);
+                console.log('filename', filename);
+                // 获取文件主体
+                let file = buf.slice(rems[i + 3] + 2, rems[i + 4]);
+                console.log('file', file);
+                // 文件流写文件
+                fs.writeFile(`${filename}`, file, (err) => {
+                    if (err) {
+                        console.log('write file fail', err)
+                    } else {
+                        console.log(`write file success: ${filename}`)
+                    }
+                });
+            }
             // 获取文件名
-            let msg = buf.slice(rems[0] + 2, rems[1]).toString();
-            let filename = msg.substring(msg.indexOf('filename=') + 10 , msg.length - 1);
-            // 获取数据主体
-            let file = buf.slice(rems[3] + 2, rems[rems.length - 2]);
-            fs.writeFile(`C:\\Users\\patrick.liu\\Desktop\\ajax\\${filename}`, file, (err) => {
-                if (err) {
-                    res.end(err.message);
-                } else {
-                    res.end('success');
-                }
-            })
+            // let msg = buf.slice(rems[0] + 2, rems[1]).toString();
+            // let filename = msg.substring(msg.indexOf('filename=') + 10 , msg.length - 1);
+            // // 获取数据主体
+            // let file = buf.slice(rems[3] + 2, rems[rems.length - 2]);
+            // fs.writeFile(`${filename}`, file, (err) => {
+            //     if (err) {
+            //         res.end(err.message);
+            //     } else {
+            //         res.end('success');
+            //     }
+            // })
             res.end('success');
         })
     }
 }).listen(3000);
+
+console.log('server started: http://localhost:3000');
